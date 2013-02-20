faec8708ddc02965b6400f7329f165eab0386caf
Miriam Sexton <miriam@industrydive.com>
2013-02-20 14:17:04 -0500

    get_all_sites fixed

--- dbsettings/models.py
+++ dbsettings/models.py
@@ -7,19 +7,21 @@ class SettingManager(models.Manager):
         all = super(SettingManager, self).get_query_set()
         return all.filter(site=Site.objects.get_current())
 
+    # Not passing in class_name, because for some reason the 
+    # class_name column is blank for every row
     def get_all_sites(self, group_obj, attribute_name):
-        return self._get_all_sites(self, group_obj.__module__, attribute_name)
+        return self._get_all_sites(group_obj.__module__, attribute_name)
         # return _get_from_all_sites(self, type(group_obj), attribute_name)
 
     def _get_all_sites(self, module_name, attribute_name):
         results = super(SettingManager, self).get_query_set().filter(
             module_name__exact=module_name,
-            attribute_name__exact=attribute_name,
-        )
+            attribute_name__exact=attribute_name
+        ).values('site_id', 'value')
 
         ret_val = dict()
-        for result in results:
-            ret_val[result.site_id] = result.value
+        for r in results:
+            ret_val[r['site_id']] = r['value']
 
         return ret_val
 
