f14ecbb596e223df2a1c534072f1e86892a18b1a
Colin Burr <cburr@industrydive.com>
2018-06-22 14:51:26 -0400

    Setting to true

--- dbsettings/settings.py
+++ dbsettings/settings.py
@@ -2,7 +2,7 @@ from django.apps import apps
 from django.conf import settings
 
 
-sites_installed = apps.is_installed('django.contrib.sites')
+sites_installed = True
 USE_SITES = getattr(settings, 'DBSETTINGS_USE_SITES', sites_installed)
 USE_CACHE = getattr(settings, 'DBSETTINGS_USE_CACHE', True)
 VALUE_LENGTH = getattr(settings, 'DBSETTINGS_VALUE_LENGTH', 255)
