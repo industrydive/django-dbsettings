e9a1d64efc0449c98c6a0ab2d725e5e8ec212508
Colin Burr <cburr@industrydive.com>
2018-06-22 14:22:34 -0400

    Add USE_SITES option from upstream

--- dbsettings/models.py
+++ dbsettings/models.py
@@ -1,24 +1,30 @@
 from django.db import models
-from django.contrib.sites.models import Site
 
+from dbsettings.settings import USE_SITES, VALUE_LENGTH
 
-class SettingManager(models.Manager):
-    def get_queryset(self):
-        sup = super(SettingManager, self)
-        qs = sup.get_queryset() if hasattr(sup, 'get_queryset') else sup.get_query_set()
-        return qs.filter(site=Site.objects.get_current())
-    get_query_set = get_queryset
+if USE_SITES:
+    from django.contrib.sites.models import Site
+
+    class SettingManager(models.Manager):
+        def get_queryset(self):
+            sup = super(SettingManager, self)
+            qs = sup.get_queryset() if hasattr(sup, 'get_queryset') else sup.get_query_set()
+            return qs.filter(site=Site.objects.get_current())
+        get_query_set = get_queryset
 
 
 class Setting(models.Model):
-    site = models.ForeignKey(Site)
+    if USE_SITES:
+        site = models.ForeignKey(Site)
+        objects = SettingManager()
+        all_sites = models.Manager()
     module_name = models.CharField(max_length=255)
     class_name = models.CharField(max_length=255, blank=True)
     attribute_name = models.CharField(max_length=255)
     value = models.CharField(max_length=255, blank=True)
 
-    objects = SettingManager()
-    all_sites = models.Manager()
+
+
 
 
     class Meta:
@@ -28,8 +34,8 @@ class Setting(models.Model):
 
     def __bool__(self):
         return self.pk is not None
-
-    def save(self, *args, **kwargs):
-        if not self.site_id:
-            self.site = Site.objects.get_current()
-        return super(Setting, self).save(*args, **kwargs)
+    if USE_SITES:
+        def save(self, *args, **kwargs):
+            if not self.site_id:
+                self.site = Site.objects.get_current()
+            return super(Setting, self).save(*args, **kwargs)
